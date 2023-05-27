e018dad25c7e638a56d1f2d4540898256b7e6808
Fedor <fedor_ka@mail.ru>
2023-05-27 17:23:02 +0300

    ok

--- CS_Basic/CS_Basic/CS_Basic.Module_3_7_4/Program.cs
+++ CS_Basic/CS_Basic/CS_Basic.Module_3_7_4/Program.cs
@@ -6,7 +6,26 @@ namespace CS_Basic.Module_3_7_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.Write("What is your favorite day of week? ");
+            DayOfWeek week_day = (DayOfWeek) int.Parse(Console.ReadLine());
+            //Console.WriteLine(week_day);
+            Console.Write("Your favorite day is {0}", week_day );
+            Console.ReadKey();
+        }
+        enum DayOfWeek 
+        { 
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
         }
     }
 }
