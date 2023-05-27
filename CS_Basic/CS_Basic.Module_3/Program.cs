b9879b2de39649baacb4f93fa5669b35eecd580b
Fedor <fedor_ka@mail.ru>
2023-05-27 16:39:22 +0300

    Start

---
+++ CS_Basic/CS_Basic.Module_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Basic.Module_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет,\n мир!");
+        }
+    }
+}
