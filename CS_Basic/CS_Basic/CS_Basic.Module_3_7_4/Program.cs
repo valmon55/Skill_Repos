88cf864b8fb2abb7edd28a4f2e50b96a74b9e824
Fedor <fedor_ka@mail.ru>
2023-05-27 17:49:55 +0300

    + bithday

--- CS_Basic/CS_Basic/CS_Basic.Module_3_7_4/Program.cs
+++ CS_Basic/CS_Basic/CS_Basic.Module_3_7_4/Program.cs
@@ -9,12 +9,17 @@ namespace CS_Basic.Module_3_7_4
             Console.Write("Enter your name: ");
             var name = Console.ReadLine();
             Console.Write("Enter your age: ");
-            var age = checked((byte)int.Parse(Console.ReadLine()));
+            //var age = checked((byte)int.Parse(Console.ReadLine()));
+            var age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-            Console.Write("What is your favorite day of week? ");
-            var week_day = (DayOfWeek) int.Parse(Console.ReadLine());
-            //Console.WriteLine(week_day);
-            Console.Write("Your favorite day is {0}", week_day );
+
+            Console.Write("Enter your birthdate: ");
+            var birthday = Console.ReadLine();
+            Console.WriteLine($"Your birthdate is {birthday}");
+            //Console.Write("What is your favorite day of week? ");
+            //var week_day = (DayOfWeek) int.Parse(Console.ReadLine());
+            //Console.Write("Your favorite day is {0}", week_day );
+
             Console.ReadKey();
         }
         enum DayOfWeek 
