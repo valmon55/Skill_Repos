a4fc8804232ce5276d79729fa0e5bf6e8f29ab42
Fedor <fedor_ka@mail.ru>
2023-06-03 16:54:37 +0300

    done

--- CS_Basic/CS_Basic.Module_4/Program.cs
+++ CS_Basic/CS_Basic.Module_4/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
+
+for (int i = 0; i < array.GetUpperBound(0) + 1; i++)
+{
+    for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
+        Console.Write(array[i, k] + " ");
+
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetUpperBound(1) + 1; i++)
+{
+    for (int k = 0; k < array.GetUpperBound(0) + 1; k++)
+        Console.Write(array[k, i] + " ");
+
+    Console.WriteLine();
+}
