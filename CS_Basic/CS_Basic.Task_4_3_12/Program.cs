362ed67f35d03dd3c1baf54f9f091958aea2b35d
Fedor <fedor_ka@mail.ru>
2023-06-03 19:39:32 +0300

    sort+

--- CS_Basic/CS_Basic.Task_4_3_12/Program.cs
+++ CS_Basic/CS_Basic.Task_4_3_12/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            for (int i=0; i < arr.Length; i++)
+                Console.Write(arr[i].ToString() + " ");
+
+            Console.WriteLine();
+
+            //int idx = 0;
+            int v;
+            for( int k=0;k<arr.Length;k++)
+                for(int i=k+1; i < arr.Length; i++)
+                    if (arr[i] < arr[k])
+                    {
+                        v = arr[k];
+                        arr[k] = arr[i];
+                        arr[i] = v;
+                    }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i].ToString() + " ");
         }
     }
 }
