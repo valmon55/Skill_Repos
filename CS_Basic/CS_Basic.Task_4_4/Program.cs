5d13bfcafecd43a40c915af66f81e2194a28897b
Fedor <fedor_ka@mail.ru>
2023-06-04 19:35:12 +0300

    start кортежи

---
+++ CS_Basic/CS_Basic.Task_4_4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CS_Basic.Task_4_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 4.4.2");
+            var anketa = (name: "Jane", age: 27);
+
+            Console.WriteLine("Введите имя: "); 
+            anketa.name = Console.ReadLine();
+            Console.WriteLine("Укажите возраст: ");
+            anketa.age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+        }
+    }
+}
