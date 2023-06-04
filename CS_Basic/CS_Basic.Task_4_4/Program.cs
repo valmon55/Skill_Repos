29c2f28cf767f36c1aa4f942dad718ea36f523a5
Fedor <fedor_ka@mail.ru>
2023-06-04 19:40:28 +0300

    4.4.3

--- CS_Basic/CS_Basic.Task_4_4/Program.cs
+++ CS_Basic/CS_Basic.Task_4_4/Program.cs
@@ -14,6 +14,18 @@
 
             Console.WriteLine("Ваше имя: {0}", anketa.name);
             Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
+            Console.WriteLine("Task 4.4.3");
+            var (name, age) = ("Jane", 27);
+
+            Console.WriteLine("Введите имя: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Укажите возраст: ");
+            age = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
         }
     }
 }
