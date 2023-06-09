a7f8b480f54aac2393e85c95239f7e8ca5fe403f
Fedor <fedor_ka@mail.ru>
2023-06-09 22:05:51 +0300

    task1

---
+++ CS_Basic/CS_Basic.Module_5_Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CS_Basic.Module_5_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            (string? Name, string?[] Dishes) User;
+            Console.WriteLine("Ваше имя?");
+            User.Name = Console.ReadLine();
+            User.Dishes = new string[5];
+            for(int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Ваше любимое блюдо?");
+                User.Dishes[i] = Console.ReadLine();
+            }
+        }
+    }
+}
