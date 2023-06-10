d43083a5a75fa3028d76db117b97f3b9ce4675ea
Fedor <fedor_ka@mail.ru>
2023-06-10 19:51:18 +0300

    5.2.8

---
+++ CS_Basic/CS_Basic.Module_5_Task_2_8/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CS_Basic.Module_5_Task_2_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static int[] GetArrayFromConsole()
+            {
+                var result = new int[5];
+
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                    result[i] = int.Parse(Console.ReadLine());
+                }
+              
+                return result;
+            }
+            var array = GetArrayFromConsole();
+            array = SortArray(array);
+            //Sort(GetArrayFromConsole());
+        }
+        static int[] SortArray(int[] array)
+        {
+            if (array.Length == 0) return array;
+
+            int t = array[0];
+            for (int k = 0; k < array.Length; k++)
+            {
+                for (int i = k + 1; i < array.Length; i++)
+                {
+                    if (array[k] > array[i])
+                    {
+                        t = array[k];
+                        array[k] = array[i];
+                        array[i] = t;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
