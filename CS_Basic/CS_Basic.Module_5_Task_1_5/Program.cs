fdf1fc7f3356ae07a14cd792ccf465a2a787ad03
Fedor <fedor_ka@mail.ru>
2023-06-10 17:57:30 +0300

    +1

--- CS_Basic/CS_Basic.Module_5_Task_1_5/Program.cs
+++ CS_Basic/CS_Basic.Module_5_Task_1_5/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string?[] favcolor = new string[3];
+            for (int i = 0; i < favcolor.Length; i++)
+            {
+                Console.WriteLine("Введите ваш цвет #" + i.ToString() + " ");
+                favcolor[i] = ShowColor();
+            }
+
+            for (int i = 0; i < favcolor.Length; i++)
+            {
+                Console.WriteLine("Ваш цвет #" + i.ToString() + " " + favcolor[i]);
+            }
+            
+        }
+        static string? ShowColor()
+        {
+            Console.Write("Введите ваш цвет ");
+            return Console.ReadLine();
         }
     }
 }
