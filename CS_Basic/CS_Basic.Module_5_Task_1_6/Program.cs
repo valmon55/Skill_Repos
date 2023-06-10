bbbc1d156f02d2aba38bce118fa663b68e3172d0
Fedor <fedor_ka@mail.ru>
2023-06-10 17:59:48 +0300

    5.1.6

---
+++ CS_Basic/CS_Basic.Module_5_Task_1_6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basic.Module_5_Task_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
