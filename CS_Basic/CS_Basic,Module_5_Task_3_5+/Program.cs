22a4ef1a169f17f771ea03586d546861055bade7
Fedor <fedor_ka@mail.ru>
2023-06-10 20:54:58 +0300

    5.3.6

---
+++ CS_Basic/CS_Basic,Module_5_Task_3_5+/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        var arr = new int[] { 1, 2, 3 };
+        BigDataOperation(arr);
+
+        Console.WriteLine(arr[0]);
+
+    }
+
+    static void BigDataOperation(in int[] arr)
+    {
+        arr[0] = 4;
+    }
+
+}
---
+++ CS_Basic/CS_Basic.Module_5_Task_3_/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CS_Basic.Module_5_Task_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var some_name = "Jane";
+            var age = 23;
+
+            Console.WriteLine("Вам {0} {1}  лет.",some_name,age);
+
+            GetName(ref some_name);
+            ChangeAge(age);
+            Console.WriteLine("Вам {0} {1}  лет.", some_name, age);
+        }
+        static void GetName(ref string name)
+        {
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+
+        }
+        static void ChangeAge(int age)
+        { 
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
