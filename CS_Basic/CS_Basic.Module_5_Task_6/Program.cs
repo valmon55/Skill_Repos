775d96b2934910ce977919e31d0c5c0c1c059793
Fedor <fedor_ka@mail.ru>
2023-06-11 18:00:21 +0300

    5.6

---
+++ CS_Basic/CS_Basic.Module_5_Task_6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CS_Basic.Module_5_Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            ShowData(InputData());
+        }
+        static (string name, string lastname, int age, bool has_pet, string[] pet_nicknames, string[] favcolors) UserData;
+        
+        static (string name, string lastname, int age, bool has_pet, string[] pet_nicknames, string[] favcolors) InputData()
+        {
+            return UserData;
+        }
+
+        static void ShowData((string name, string lastname, int age, bool has_pet, string[] pet_nicknames, string[] favcolors) user_data)
+        { 
+            
+        }
+    }
+}
