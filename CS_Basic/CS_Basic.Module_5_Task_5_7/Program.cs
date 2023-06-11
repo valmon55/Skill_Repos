6161e7af5a3c1fdcbd97fc8a78e83226271e482f
Fedor <fedor_ka@mail.ru>
2023-06-11 17:09:23 +0300

    5.5.7

---
+++ CS_Basic/CS_Basic.Module_5_Task_5_7/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CS_Basic.Module_5_Task_5_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(20));
+        }
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
