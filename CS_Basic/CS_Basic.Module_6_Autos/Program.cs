0d6522d416338f46c3c0f39a178808fd6c3fd398
valmon55 <fedor_ka@mail.ru>
2023-06-24 20:34:24 +0300

    Module 6 Autos

---
+++ CS_Basic/CS_Basic.Module_6_Autos/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace CS_Basic.Module_6_Autos
+{
+    class ElectricEngine { }
+    class GasEngine { }
+    class Battery { }
+    public class Differential { } 
+    public class Wheel { }
+    class Car<T1>
+    { 
+        T1 Engine;
+        Battery battery;
+        Differential differential;
+        Wheel wheel;
+
+        public virtual void ChangePart<T2>(T2 newPart)
+        { }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
