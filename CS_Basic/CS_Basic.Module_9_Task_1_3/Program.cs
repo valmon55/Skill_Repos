912bfc000e22794509205a39e57818d02bdc05f1
valmon55 <fedor_ka@mail.ru>
2023-07-08 12:49:19 +0300

    task 9.2.3

--- CS_Basic/CS_Basic.Module_9_Task_1_3/Program.cs
+++ CS_Basic/CS_Basic.Module_9_Task_1_3/Program.cs
@@ -9,6 +9,7 @@
             //task 9.1.4
             Exception exception1= new Exception("Сообщение об ошибке");
             exception1.HelpLink = "www.ya.ru";
+            //task 9.2.2
             Exception exception2 = new ArgumentOutOfRangeException();
             try
             {
@@ -24,6 +25,25 @@
             {
                 Console.WriteLine("Block finally");
             }
+            //task 9.2.3
+            //Exception excep23 = new ArgumentOutOfRangeException();
+            try
+            {
+                Console.WriteLine("Block try");
+                throw new RankException("Ошибка Rank Exception!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Block catch");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.GetType().ToString());
+
+            }
+            finally
+            {
+                Console.WriteLine("Block finally");
+            }
+
         }
     }
 }
