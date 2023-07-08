6fc0ff402aa9883a726c6fcf86f29cf0864006da
valmon55 <fedor_ka@mail.ru>
2023-07-08 12:13:39 +0300

    task_9.1.3

---
+++ CS_Basic/CS_Basic.Module_9_Task_1_3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basic.Module_9_Task_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
