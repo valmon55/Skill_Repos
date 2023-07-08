d7bc8d277f16c1a2d10655ad4520811397bb015b
valmon55 <fedor_ka@mail.ru>
2023-07-08 12:43:47 +0300

    9.2.2

--- CS_Basic/CS_Basic.Module_9_Task_1_3/Program.cs
+++ CS_Basic/CS_Basic.Module_9_Task_1_3/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Exception exception = new Exception();
+            exception.Data.Add("Дата создания исключения",DateTime.Now);        
+            //task 9.1.4
+            Exception exception1= new Exception("Сообщение об ошибке");
+            exception1.HelpLink = "www.ya.ru";
+            Exception exception2 = new ArgumentOutOfRangeException();
+            try
+            {
+                Console.WriteLine("Block try");
+                throw exception2;
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Block catch");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Block finally");
+            }
         }
     }
 }
