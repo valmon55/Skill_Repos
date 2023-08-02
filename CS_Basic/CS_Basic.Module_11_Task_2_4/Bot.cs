b920a06331b9f1132a96189e6f8090dfed726ece
valmon55 <fedor_ka@mail.ru>
2023-08-02 10:12:08 +0300

    Module 11 task start

---
+++ CS_Basic/CS_Basic.Module_11_Task_2_4/Bot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Basic.Module_11_Task_2_4
+{
+    internal class Bot
+    {
+    }
+}
---
+++ CS_Basic/CS_Basic.Module_11_Task_2_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CS_Basic.Module_11_Task_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
