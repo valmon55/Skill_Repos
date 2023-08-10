e5b8a9858ed9c352c3b36e49bbee2f1c50aa94a4
valmon55 <fedor_ka@mail.ru>
2023-08-10 17:58:46 +0300

    Count Words start

---
+++ CS_Basic/CS_Basic.Module_13_Tasks/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CS_Basic.Module_13_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            using (StreamReader sr = new StreamReader(@"D:\Temp\module13_task_1_6.txt"))
+            {
+                text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Task 13.1.6
+        /// </summary>
+        /// <param name="ints"></param>
+        /// <returns></returns>
+        static int CountWords(string text)
+        {
+            return 0;
+        }
+
+        static bool IsSorted(int[] ints)
+        {
+            if(ints.Length == 0) 
+                return false;
+            //int i = ints[0];
+            for(int i = 1; i < ints.Length; i++) 
+            {
+                if (ints[i] <= ints[i-1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
