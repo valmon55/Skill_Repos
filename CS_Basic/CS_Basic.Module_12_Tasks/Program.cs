c90cedf64ca8b31deb41396edb552f71d15f4ce4
valmon55 <fedor_ka@mail.ru>
2023-08-10 15:00:12 +0300

    module 12

---
+++ CS_Basic/CS_Basic.Module_12_Tasks/Program.cs
@@ -0,0 +1,117 @@
+﻿using BenchmarkDotNet.Running;
+using System.Diagnostics;
+
+namespace CS_Basic.Module_12_Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Оценка времени выполнения Медиана
+            Estimate(20);
+            //var summary = BenchmarkRunner.Run<Testing>();
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// StopWatch
+        /// </summary>
+        /// <param name="n"></param>
+        static void CreateMatrix(int n)
+        {
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = i + j;
+                }
+            }
+        }
+
+        static void Estimate(int n)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            for (int i = 0; i < n; i++)
+            {
+                timer.Restart();
+
+                CreateMatrix(10000);
+
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+            }
+        }
+        //12.3.1
+        static int BinarySearch(int value, int[] array, int left, int right)
+        {
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+
+                var midElement = array[middle];
+
+                if (midElement == value)
+                {
+                    return middle;
+                }
+                else if (value < midElement)
+                {
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+
+            return -1;
+        }
+        static int BinarySearchRecursive(int value, int[] array, int left, int right)
+        {
+
+            var middle = (left + right) / 2;
+
+            var midElement = array[middle];
+
+            if (midElement == value)
+            {
+                return middle;
+            }
+            else if (left < right)
+            {
+                if (value < midElement)
+                {
+                    return BinarySearch(value, array, left, middle - 1);
+                }
+                else
+                {
+                    return BinarySearch(value, array, middle + 1, right);
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        //12.3.3
+        static int GetIndex(int[] array, int element)
+        {
+            int index;
+            for (index = 0; index < array.Length; index++)
+            {
+                if (array[index] > element)
+                    return index;
+            }
+
+            return index;
+        }
+    }
+}
---
+++ CS_Basic/CS_Basic.Module_12_Tasks/Testing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace CS_Basic.Module_12_Tasks
+{
+    public class Testing
+    {
+        [Benchmark]
+        public void CreateMatrix()
+        {
+            int n = 10000;
+
+            var matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = i + j;
+                }
+            }
+        }
+    }
+}
