80d3de322488d989fe0c38f6f4dc400194ae7d43
valmon55 <fedor_ka@mail.ru>
2023-08-25 22:55:42 +0300

    immediate ?

---
+++ LINQ_part_2/LINQ_Part_2/LINQ.Postponed_vs_Immediately/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LINQ.Postponed_vs_Immediately
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int>() { 1, 2, 3, 4 };
+            var num_check = from n in nums
+                            where (n < 3)
+                            select n;
+            var num_count = (from n in nums
+                             where (n < 3)
+                             select n).ToArray();
+            nums.Add(-1);
+            Console.WriteLine("num_check:");
+            foreach (var s in num_check)
+                Console.WriteLine(s);
+
+            Console.WriteLine("num_count:");
+            foreach (var s in num_count)
+                Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
