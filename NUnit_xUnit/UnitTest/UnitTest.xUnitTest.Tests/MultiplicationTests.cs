06907b4b9c823edb42c7105bd8ed346c0b40a792
valmon55 <fedor_ka@mail.ru>
2023-09-06 12:18:11 +0300

    only NUnit

---
+++ NUnit_xUnit/UnitTest/UnitTest.xUnitTest.Tests/MultiplicationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.xUnitTest;
+
+
+namespace UnitTest.xUnitTest.Tests
+{
+    public class MultiplicationTests
+    {
+        
+        public void MultiplicatonMustReturnNotNull()
+        {
+            var calc = new 
+
+            Assert
+        }
+    }
+}
---
+++ NUnit_xUnit/UnitTest/UnitTest.xUnitTest/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.xUnitTest
+{
+    public class Calculator
+    {
+        public int Multiplication(int a, int b) 
+        {  
+            return a * b;
+        }
+    }
+}
