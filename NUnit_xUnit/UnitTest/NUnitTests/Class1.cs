e86ba861135058f9902db97f9220989332253cc2
valmon55 <fedor_ka@mail.ru>
2023-09-06 12:49:34 +0300

    NUnit

---
+++ NUnit_xUnit/UnitTest/NUnitTests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        { 
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
