9486d52667a5118f1e6d765044764219cd0afa7f
valmon55 <fedor_ka@mail.ru>
2023-09-06 17:53:15 +0300

    mock

---
+++ NUnit_xUnit/Mock.UserRepository/Mock.UserRepository/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mock.UserRepository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
